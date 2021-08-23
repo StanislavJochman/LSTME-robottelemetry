9f2b2f7dfd6425b82ec6b021c73854265fa0301d
Michal Puškel <puskel@freevision.sk>
2021-08-23 19:14:59 +0200

    change setup, add logo

--- PyQt/setup.py
+++ PyQt/setup.py
@@ -1,17 +1,16 @@
 from cx_Freeze import setup, Executable
 
-executables = [Executable("test.py",icon="../img/LSTME_logo.png")]
+executables = [Executable("main.py", icon="LSTME_logo.png")]
 
 setup(
     name="JozefBot Telemetry",
-    version = "1.0.0",
-    description = 'Telemetry program for JozefBot',
-    author = 'Stanislav Jochman',
-    options={"build_exe": {"packages":["serial","PyQt5","time","sys","serial.tools.list_ports","time","ui"],
-                           "include_files":["../img/gyro.png",
-                                            "../img/robot.png",
-                                            "../img/LSTME_logo.png"
-                                            ]}},
-    executables = executables
-    )
-
+    version="1.0.0",
+    description='Telemetry program for JozefBot',
+    author='Stanislav Jochman',
+    options={"build_exe": {"packages": ["serial", "PyQt5", "time", "sys", "serial.tools.list_ports", "time", "ui"],
+                           "include_files": ["../img/gyro.png",
+                                             "../img/robot.png",
+                                             "../img/LSTME_logo.png"
+                                             ]}},
+    executables=executables
+)
